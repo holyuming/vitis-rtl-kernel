bd15e79d73e6a89df134a4ab3097d572b5a86acb
holyuming <holyuming.tw@gmail.com>
2024-01-25 00:47:15 +0800

    modify user-host.cpp to test multiple data

--- binary2gray/src/host/user-host.cpp
+++ binary2gray/src/host/user-host.cpp
@@ -12,7 +12,7 @@
 #include <experimental/xrt_xclbin.h>
 #include <experimental/xrt_ip.h>
 
-#define DATA_SIZE 2 
+#define DATA_SIZE 100
 #define IP_START 0x1
 #define IP_DONE 0x2
 #define IP_IDLE 0x4
@@ -59,12 +59,15 @@ int main(int argc, char** argv) {
 
     // Create the test data
     int bufReference[DATA_SIZE];
-    bo0_map[0] = 456;
-    std::cout << "Input Binary Code: " << bo0_map[0] << std::endl;
+    for (int i=0 ; i<DATA_SIZE; i++) {
+        bo0_map[i] = i;
+    }
 
 
     //Generate gold data data for validation
-    bufReference[0] = (bo0_map[0] >> 1) ^ bo0_map[0];
+    for (int i=0 ; i<DATA_SIZE; i++) {
+        bufReference[i] = (bo0_map[i] >> 1) ^ bo0_map[i];
+    }
 
 
     // Get the buffer physical address
@@ -78,60 +81,73 @@ int main(int argc, char** argv) {
     ip1_boA.sync(XCL_BO_SYNC_BO_TO_DEVICE);
     ip1_boB.sync(XCL_BO_SYNC_BO_TO_DEVICE);
 
-
-    // Write Binary code address
-    std::cout << "INFO: Setting IP Data" << std::endl;
-    std::cout << "Setting Register \"Binary code\" (Input Address)" << std::endl;
-    ip1.write_register(Bin_OFFSET, buf_addr[0]);
-    ip1.write_register(Bin_OFFSET + 4, buf_addr[0] >> 32);
+    
+    // start simulation
     uint32_t tmp0, tmp1, adr;
-    tmp0 = ip1.read_register(Bin_OFFSET);
-    tmp1 = ip1.read_register(Bin_OFFSET + 4);
-    adr = buf_addr[0];
-    std::cout << "Gold 0x1c: " << std::hex << adr           		<< " Your: " << std::hex << tmp0 << std::endl;
-    std::cout << "Gold 0x20: " << std::hex << (buf_addr[0] >> 32)   << " Your: " << std::hex << tmp1 << std::endl;
-
-
-    // Write Gray code address
-    std::cout << "Setting Register \"Gray Code\" (Input Address)" << std::endl;
-    ip1.write_register(Gry_OFFSET, buf_addr[1]);
-    ip1.write_register(Gry_OFFSET + 4, buf_addr[1] >> 32);
-    tmp0 = ip1.read_register(Gry_OFFSET);
-    tmp1 = ip1.read_register(Gry_OFFSET + 4);
-    adr = buf_addr[1];
-    std::cout << "Gold 0x1c: " << std::hex << adr           		<< " Your: " << std::hex << tmp0 << std::endl;
-    std::cout << "Gold 0x20: " << std::hex << (buf_addr[1] >> 32)   << " Your: " << std::hex << tmp1 << std::endl;
-
-
-    // Start kernel
-    std::cout << "INFO: IP Start" << std::endl;
-    ip1.write_register(USER_OFFSET, IP_START);
-
-    // Polling 
     uint32_t axi_ctrl = 0, krnl_done = 0, krnl_idle = 0;
-    int i = 0;
-    while (krnl_done != IP_DONE) {
-        axi_ctrl = ip1.read_register(USER_OFFSET);
-        krnl_done = axi_ctrl & 0x02;
-        krnl_idle = axi_ctrl & 0x04;
-        i = i + 1;
-        std::cout << "Read Loop iteration: " << i << " Kernel Done: " << krnl_done << " Kernel Idle: " << krnl_idle << "\n";
+    int j = 0;
+    for (int i=0; i<DATA_SIZE; i++) {
+        // Write Binary code address
+        std::cout << "INFO: Setting IP Data" << std::endl;
+        std::cout << "Setting Register \"Binary code\" (Input Address)" << std::endl;
+        ip1.write_register(Bin_OFFSET, buf_addr[0]);
+        ip1.write_register(Bin_OFFSET + 4, buf_addr[0] >> 32);
+        tmp0 = ip1.read_register(Bin_OFFSET);
+        tmp1 = ip1.read_register(Bin_OFFSET + 4);
+        adr = buf_addr[0];
+        std::cout << "Gold 0x1c: " << std::hex << adr           		<< " Your: " << std::hex << tmp0 << std::endl;
+        std::cout << "Gold 0x20: " << std::hex << (buf_addr[0] >> 32)   << " Your: " << std::hex << tmp1 << std::endl;
+
+
+        // Write Gray code address
+        std::cout << "Setting Register \"Gray Code\" (Output Address)" << std::endl;
+        ip1.write_register(Gry_OFFSET, buf_addr[1]);
+        ip1.write_register(Gry_OFFSET + 4, buf_addr[1] >> 32);
+        tmp0 = ip1.read_register(Gry_OFFSET);
+        tmp1 = ip1.read_register(Gry_OFFSET + 4);
+        adr = buf_addr[1];
+        std::cout << "Gold 0x1c: " << std::hex << adr           		<< " Your: " << std::hex << tmp0 << std::endl;
+        std::cout << "Gold 0x20: " << std::hex << (buf_addr[1] >> 32)   << " Your: " << std::hex << tmp1 << std::endl;
+
+
+        // Start kernel
+        std::cout << "INFO: IP Start" << std::endl;
+        ip1.write_register(USER_OFFSET, IP_START);
+
+        // Polling 
+        while (krnl_done != IP_DONE) {
+            axi_ctrl = ip1.read_register(USER_OFFSET);
+            krnl_done = axi_ctrl & 0x02;
+            krnl_idle = axi_ctrl & 0x04;
+            j += 1;
+            std::cout << "Read Loop iteration: " << j << " Kernel Done: " << krnl_done << " Kernel Idle: " << krnl_idle << "\n";
+        }
+        std::cout << "INFO: IP Done" << std::endl;
+
+        buf_addr[0] += 4;
+        buf_addr[1] += 4;
     }
 
-    std::cout << "INFO: IP Done" << std::endl;
-
     // Get the output;
+    std::cout << std::dec;
     std::cout << "Get the output data from the device" << std::endl;
     ip1_boB.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
 
     // Validate results
-    uint32_t out = bo1_map[0], gold = bufReference[0];
+    uint32_t out[DATA_SIZE], gold[DATA_SIZE];
+    int error_num = 0;
 
-    std::cout << std::dec;
-    std::cout << "Gold: " << gold   << std::endl;
-    std::cout << "Your: " << out    << std::endl;
+    for (int i=0; i<DATA_SIZE; i++) {
+        out[i]  = bo1_map[i];
+        gold[i] = bufReference[i];
 
-    if (out != gold)
+        if (out[i] != gold[i])
+            error_num += 1;
+
+        std::cout << "Binary: " << bo0_map[i] << ", Gold: " << gold[i] << ", Your: " << out[i] << std::endl;
+    }
+
+    if (error_num != 0)
     	std::cout << "TEST FAILED\n";
     else
     	std::cout << "TEST PASSED\n";
